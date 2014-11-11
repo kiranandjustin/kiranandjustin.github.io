62ffe94a1b290cee879246d1a864e90ea5c61eaf
Pratik <pratik.rathod@gmail.com>
2014-11-10 21:25:37 -0500

    update rsvp - still need to fix dietary restrictions

--- js/firebaselib.js
+++ js/firebaselib.js
@@ -3,9 +3,9 @@ var ref = new Firebase("https://sweltering-inferno-5630.firebaseio.com/invite_li
 
 var nameForm = "\
 <div id='lookup_rsvp_in'  class='col-xs-12 form-group controls'> \
-    <div id='lookup_names'>Pratik Rathod</div> \
-    <input type='checkbox' name='rsvp_wedding' value='Car'>Wedding</input> \
-    <input type='checkbox' name='rsvp_reception' value='Car'>Reception</input> \
+    <div id='lookup_names'>Pratik Rathod</div>\
+    <input type='checkbox' name='rsvp_wedding' value='rsvp_wedding'>Wedding</input> \
+    <input type='checkbox' name='rsvp_reception' value='rsvp_reception'>Reception</input> \
     <select name='rsvp_diet' id='rsvp_diet' class='rsvp_diet'> \
       <option value='none'>Not Applicable</option> \
       <option value='veggie'>Vegetarian</option> \
@@ -30,11 +30,15 @@ function get_postal(snapshot) {
       
     });
 }
+
+var family = [];
 function get_family() {
   var first = document.getElementById("firstname").value
+  first = first.charAt(0).toUpperCase() + first.substring(1);
   var last = document.getElementById("lastname").value;
+  last = last.charAt(0).toUpperCase() + last.substring(1);
   var name = first + " " + last;
-  ref.orderByChild("full_name").equalTo(name).limitToLast(1).on("value", function(snapshot) {
+  ref.orderByChild("full_name").equalTo(name).limitToLast(1).once("value", function(snapshot) {
     if (snapshot.numChildren() == 0) {
       console.log("user not found");
     }
@@ -50,17 +54,15 @@ function get_family() {
         }
       });  
       
-      ref.orderByChild("postal_code").equalTo(postalcode).on("value", function(familysnap) {
+      ref.orderByChild("postal_code").equalTo(postalcode).once("value", function(familysnap) {
         console.log("lookup based on postal: " + familysnap.val());
         $.each( familysnap.val(), function( index, person ) {
           if (person) {
             var formhtml = $.parseHTML(nameForm);
-            console.log('person: ' + person);
-            var name = person.first_name + " " + person.last_name;
-            if (person.type === "guest") {
-              name += " (Guest)";
-            }
+            var name = person.full_name;
+            if (person.type === "guest") { name += " (Guest)"; }
             $(formhtml).find('#lookup_names').text(name);
+            family[name] = index; // save key to global variable.
             $('#rsvpForm .lookup_info').append($(formhtml));
           }
         });
@@ -84,13 +86,34 @@ function setDietaryList() {
        $(this).next().hide();
      }
    });
+   
+   
    $('#rsvpForm').submit(function() {
+     console.log('hello world');
      // send back to firebase (set params)
-       var othersOption = $('#rsvp_diet').find('option:selected');
+     var people_form = $('#rsvpForm #lookup_rsvp_in');
+     $.each( people_form, function( index, person ) {
+       var name = $(person).find('#lookup_names').text();
+       var othersOption = $(person).find('#rsvp_diet').find('option:selected');
        if(othersOption.val() == "other")
-       {
-           // replace select value with text field value
-           othersOption.val($("#rsvp_other_diet").val());
-       }
+        {
+            // replace select value with text field value
+            othersOption.val($("#rsvp_other_diet").val());
+        }
+        var up_person = new Firebase("https://sweltering-inferno-5630.firebaseio.com/invite_list/" + family[name]);
+        if (up_person) {
+          debugger;
+          up_person.update({
+             "rsvp_wedding":$(person).find('input[name=rsvp_wedding]').is(':checked'),
+             "rsvp_reception":$(person).find('input[name=rsvp_reception]').is(':checked'),
+             "rsvp_diet":othersOption.val(),
+             "rsvp_complete":true
+           });
+        }
+        else {
+          console.log('new user');
+        }
+       
+     });
    });
 };
